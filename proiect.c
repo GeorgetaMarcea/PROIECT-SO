19cb4bb6c3287c5ad348cf09187cdd1b86cdba70
Geti <user@MyDebian>
2024-04-16 21:18:25 +0300

    Am rezolvat problema cu arg[2]

--- proiect.c
+++ proiect.c
@@ -13,8 +13,7 @@
 
 #define PATH_MAX 4096
 
-void parcurgere_director(char *nume_director, int snapshot, int nivel)
-{
+void parcurgere_director(char *nume_director, int snapshot, int nivel){
     DIR *dir;
     struct dirent *intrare;
     struct stat info;
@@ -27,8 +26,7 @@ void parcurgere_director(char *nume_director, int snapshot, int nivel)
     memset(spatii, ' ', 2 * nivel);
     spatii[2 * nivel] = '\0';
     
-    if(!(dir = opendir(nume_director))) //verificam daca putem deschide directorul 
-    {
+    if(!(dir = opendir(nume_director))){
         perror("Eroare la deschiderea directorului din functia 'parcurgere_director");
         exit(-1);
     }
@@ -36,32 +34,25 @@ void parcurgere_director(char *nume_director, int snapshot, int nivel)
     sprintf(mesaj, "%sDIRECTOR: %s\n", spatii, nume_director);
     write(snapshot, mesaj, strlen(mesaj));
 
-    while(((intrare = readdir(dir)) != NULL)) //parcurgem directorul
-    {
+    while(((intrare = readdir(dir)) != NULL)){
         if(strcmp (intrare->d_name, ".") ==0 || strcmp (intrare->d_name, "..")==0)
             continue;
 
         snprintf(cale, sizeof(cale), "%s/%s", nume_director, intrare->d_name);
 
-        if((lstat(cale, &info)) < 0)   // stat->citeste atributele unui fisier
-        {                               //daca e <0  inseamna nu avem atribute
-        
+        if((lstat(cale, &info)) < 0){
             //folosim lstat->daca avem leg simbolica, stat nu afiseaza atributele din legatura, le va afisa din fisier
             perror("Eroare la lstat\n");
             exit(1);
         }
 
-        if(S_ISDIR(info.st_mode))  //daca e director
-        {
+        if(S_ISDIR(info.st_mode)){
             sprintf(mesaj, "    %s ---> DIRECTOR ---> Dimeniune %d bytes ---> Last access time %s", cale, intrare->d_reclen, ctime(&info.st_atime));
             write(snapshot, mesaj, strlen(mesaj));
             write(snapshot, "\n", strlen("\n"));
             parcurgere_director(cale, snapshot, nivel + 1);
-        }
-        else
-        {
-            if(S_ISLNK(info.st_mode))  //daca e leg simbolica
-            {
+        }else{
+            if(S_ISLNK(info.st_mode)){
                 sprintf(mesaj, "    %s ---> LINK SIMBOLIC ---> Dimeniune %d bytes ---> Last access time %s", cale, intrare->d_reclen, ctime(&info.st_atime));
                 write(snapshot, mesaj, strlen(mesaj));
                 n=readlink(cale, cale_link, sizeof(cale_link));  //citim continutul leg simbolice
@@ -69,24 +60,18 @@ void parcurgere_director(char *nume_director, int snapshot, int nivel)
                 sprintf(mesaj, "%s  %s -> %s\n", spatii, cale, cale_link);
                 write(snapshot, mesaj, strlen(mesaj));
                 write(snapshot, "\n", strlen("\n"));
-            }
-            else
-            {
+            }else{
                 sprintf(mesaj, "    %s ---> REGULAR FILE ---> Dimeniune %d bytes ---> Last access time %s", cale, intrare->d_reclen, ctime(&info.st_atime));
                 write(snapshot, mesaj, strlen(mesaj));
                 write(snapshot, "\n", strlen("\n"));
-
             }
-     
         }
     }
-
-       closedir(dir);
+    closedir(dir);
 }
 
 //Functia aceasta ne denumeste fiecare fisier sub forma: "snapshot_[numele_directorului].txt"
-char* numeFisier(char *director)
-{
+char* numeFisier(char *director){
     char *nume = basename(director); //functia basename ne da numele de baza al directorului
 
     static char fisier[100];  //avem nevoie de static pentru ca variabila noastra sa ramana in memorie 
@@ -95,74 +80,67 @@ char* numeFisier(char *director)
     return fisier;
 }
 
-int main( int argc, char **argv )
-{
+int main( int argc, char **argv ){
+    
     DIR *d;
     int ok;
+    int output;
+    char *nume_output;
 
-    if((argc < 3) || (argc > 10))
-    {
+    if((argc < 3) || (argc > 10)){
         perror("Numar de argumente invalid\n");
         exit(-1);
-    }
-    else
-    {
+    }else{
         ok = 0;
-        for(int i = 3; i < argc - 1; i++)  //verificam daca sunt distincte
-        {
-            for(int j = i+1; j < argc; j++)
-            {
-                if((strcmp(argv[i], argv[j])) == 0)  //daca avem arg egale ok va deveni 1
-                {
+        for(int i = 3; i < argc - 1; i++){
+            for(int j = i+1; j < argc; j++){ 
+                if((strcmp(argv[i], argv[j])) == 0){
                     ok = 1;
-                    perror("Argumente egale in linia de comanda\n");
+                    perror("Argumente egale in linia de comanda\n"); 
                     exit(-1);
                 }
             }
-
         }
     }
 
-        //aici ma gandeam ca trebuie sa verificam si daca directorul are numele SNAPSHOT-uri(in cazul meu), ca asa ii putem da oricare director si va functiona oricum
-     if((ok == 0) && ((strcmp(argv[1], "-o")) == 0) && ((strcmp(argv[2], "SNAPSHOT-uri")) == 0))  //daca sunt arg distincte, atunci nu se intra in if-ul de mai sus si ok nu devine 1
-    {                                           //verificam si daca argv[1] e -o
-        if((d = opendir(argv[2])) == NULL) //verificam daca argv[2] e un director
-        {
-            perror("Argumentul 2 din linia de comanda nu este director\n");
-            exit(-1);
-        } 
+    nume_output=argv[2];
 
-        for(int i = 3; i < argc; i++)
-        {
-            if((d = opendir( argv[i] )) == NULL) //verificam daca argumentele din linia de comanda sunt directoare
-            {
+    if((d = opendir(nume_output)) != 0){    //verificam daca exita directorul nostru, iar in caz ca nu exista creem un director cu numele argumentului 2
+        perror("Directorul exista\n");
+    }
+    else{
+        output = mkdir(nume_output, 0777);   //0777 este un cod de permisiuni pentru noul director
+
+        if(output != 0){    //verificam daca s a creat cu succes
+            perror("Eroare la crearea directorului\n");
+        }
+    }
+
+    if((ok == 0) && ((strcmp(argv[1], "-o")) == 0)){
+        for(int i = 3; i < argc; i++){
+            if((d = opendir( argv[i] )) == NULL)
                 continue;
-            }
-            else
-            {
+            else{
 
-                if((chdir("SNAPSHOT-uri")) == -1)
-                {
-                    perror("Nu exista directorul SNAPSHOT-uri\n");
+                if((chdir(nume_output)) == -1){ //Dar unde se afla acest director ? De regula variabele statice, se extrag intr-un fisier aparte, si din acesta se extrag date )
+    //!!!   aici am mi am schimbat calea in directorul dat ca parametru
+    //      de exemplu: eu am fost in PROIECT-SO si acum sunt in nume_output (SNAPSHOT-uri in cazul meu) 
+                    perror("Nu exista directorul\n");
+                    exit(-1);
+                }
+                int snapshot_director; 
+                if((snapshot_director = open(numeFisier(argv[i]), O_CREAT | O_RDWR, S_IWUSR | S_IRUSR | S_IXUSR)) == -1){
+                    perror("Eroare la deschideree\n");
+                    exit(-1);
+                }
+                if((chdir("..")) == -1){ //Aici nu am inteles ce se intampla !
+    //!!!   aici revin in directorul meu PROIECT-SO
                     exit(-1);
                 }
 
-                int f;
+                parcurgere_director(argv[i], snapshot_director, 0);
 
-                if((f = open(numeFisier(argv[i]), O_CREAT | O_RDWR, S_IWUSR | S_IRUSR | S_IXUSR)) == -1)  //am folosit O_CREAT pentru a-l crea si O_RDWR pentru a putea scrie/citi in/din el, iar S_IWUSR si celelate trebuie folosite, deoarece cu O_CREAT trebuie sa adaugam si param mode in functia open
-                {
-                    perror("eroare la deschideree\n");
-                    exit(-1);
-                }
-
-                if((chdir("..")) == -1)
-                {
-                    exit(-1);
-                }
-
-                parcurgere_director(argv[i], f, 0);
-
-                close(f);
+                close(snapshot_director);
             }
         }
     }
